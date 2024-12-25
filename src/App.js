9fefe53dae0bfd8b983ba24f2b5127c4a73c01e1
Raman2407kmr <raman2407kumar@gmail.com>
2024-12-25 23:39:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     console.log("Host URL"+process.env.PUBLIC_URL);
     return (
 
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename={"My Base from branch B"}>
         <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
